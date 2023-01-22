4b4facd509410bb4bd044e9940528303f3182460
Prajil <Prajil7425@gmail.com>
2023-01-22 18:50:26 +0530

    Update key style

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener('keydown', function(e){
     console.log(e.keyCode);
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if (!audio) return;
     audio.play();
 })
