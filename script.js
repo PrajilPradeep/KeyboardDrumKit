8b3917358073f3bc9e5a1f0bdd1cd88023d5ecb0
Prajil <Prajil7425@gmail.com>
2023-01-22 18:52:56 +0530

    Add option for the sound to start on keypress

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 document.addEventListener('keydown', function(e){
     console.log(e.keyCode);
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return;
+    audio.currentTime = 0; //to rewind to start
     audio.play();
+    console.log(key)
+    key.classList.add('playing')
 })
