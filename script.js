e6c961333e593527c93185cec3943e7c1781f632
Prajil <Prajil7425@gmail.com>
2023-01-23 21:08:18 +0530

    Make separation function for keydown event

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-document.addEventListener('keydown', function(e){
-    // console.log(e.keyCode);
+function playSound(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return;
     audio.currentTime = 0; //to rewind to start
     audio.play();
-    // console.log(key)
     key.classList.add('playing');
-})
+}
+document.addEventListener('keydown', playSound)
 function removeTransition(e){
     if (e.propertyName!='transform') return; //skip it if it's not a transform
-    // console.log(e.propertyName);
     this.classList.remove('playing');
 }
 const keys = document.querySelectorAll('.key');
