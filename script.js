884376466f9efe51af105bb05f219241fcdf738f
Prajil <Prajil7425@gmail.com>
2023-01-23 21:04:07 +0530

    Remove style applied to keys after transition

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 document.addEventListener('keydown', function(e){
-    console.log(e.keyCode);
+    // console.log(e.keyCode);
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if (!audio) return;
     audio.currentTime = 0; //to rewind to start
     audio.play();
-    console.log(key)
-    key.classList.add('playing')
+    // console.log(key)
+    key.classList.add('playing');
 })
+function removeTransition(e){
+    if (e.propertyName!='transform') return; //skip it if it's not a transform
+    // console.log(e.propertyName);
+    this.classList.remove('playing');
+}
+const keys = document.querySelectorAll('.key');
+keys.forEach(k=>k.addEventListener('transitionend',removeTransition))
